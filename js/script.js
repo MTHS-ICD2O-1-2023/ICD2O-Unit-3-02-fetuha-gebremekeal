7fb4a641ec8ca5f911f47541fe0b952a49f1ad93
Fethawit <159185142+fetuha-gebremekeal@users.noreply.github.com>
2024-04-03 18:32:55 +0000

    js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Fetuha
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates volume.
+ */
+function calculateVolumeOfPyramid() {
+  // input
+  const baseLength = parseInt(document.getElementById('base-length').value)
+  const baseWidth = parseInt(document.getElementById('base-width').value)
+  const heighOfPyramid = parseInt(document.getElementById('height').value)
+
+  // process
+  const volumeOfPyramid = (baseLength * baseWidth * heighOfPyramid) / 3
+
+  // output
+  document.getElementById("volume").innerHTML = 'Volume is: ' + volumeOfPyramid + ' m'
+}
